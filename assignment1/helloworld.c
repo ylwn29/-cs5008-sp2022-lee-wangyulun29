09e7e4cf41269726ed3205018f12bb91ef31d0c8
Yulun Wang <ylwn29@login-students.ccs.neu.edu>
2022-01-24 18:44:05 -0500

    upload assignment 1

--- assignment1/helloworld.c
+++ assignment1/helloworld.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-// name: Jeongkyu Lee 
-// email: jeo.lee@northeastern.edu
+// name: Yulun Wang
+// email: wang.yulun@northeastern.edu
 
 int main() {
-  printf("Hello World.  This is [YourName].\n");
-  printf("I am assignment 1 for [This Semester].\n");
+  printf("Hello World.  This is Yulun Wang.\n");
+  printf("I am assignment 1 for Spring 2022.\n");
   return 0;
 }
