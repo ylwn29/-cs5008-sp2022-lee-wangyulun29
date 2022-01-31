dbfbc0f6d9fa09e995951ee3f76b998b39362f6a
pkasaragod <83485423+pkasaragod@users.noreply.github.com>
2022-01-31 11:00:26 -0800

    Add files via upload

---
+++ labassignment1/employee.c
@@ -0,0 +1,32 @@
+/*C Program to read and print the employee details using structure and dynamic memory allocation*/
+#include <stdio.h>
+#include <stdlib.h>
+/*structure to hold the details of an employee*/
+typedef struct employee
+{
+    int empId;
+    char Name[100];
+    char Designation[100];
+    char Dept[10];
+
+}employee_t;
+/* Read the employee details using scanf*/
+void readData(int n, employee_t* s)
+{
+    
+}
+/* Function to print the employee details*/
+void display(int n, employee_t* s)
+{
+    
+    
+}
+
+/*----------Main Program--------*/
+int main()
+
+{
+    
+    return 0;
+}
+
