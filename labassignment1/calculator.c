8ddeff497b7c81da0666490a6373e85262ab8491
wangyulun29 <wang.yulun@northeastern.edu>
2022-02-05 00:24:00 -0800

    upload labassignment1-1

--- labassignment1/calculator.c
+++ labassignment1/calculator.c
@@ -1,5 +1,5 @@
-//enter your name here
-//enter your email here
+//Yulun Wang
+//wang.yulun@northeastern.edu
 #include<stdio.h>
 int main()
 {
@@ -12,7 +12,30 @@ int main()
     printf("Enter a and b values");
     scanf("%d %d", &a, &b);
     
-    
-           
+    switch(choice) {
+      case 1:
+        c = a + b;
+	printf("Addtion\nSum = %d\n", c);
+	break;
+      case 2:
+	c = a - b;
+	printf("Subtraction\nDifference = %d\n", c);
+	break;
+      case 3:
+	c = a * b;
+	printf("Multiplication\nProduct = %d\n", c);
+	break;
+      case 4:
+	if (b == 0) {
+	  printf("ZeroDivisionError!\n");
+	  break;
+	}
+	c = a / b;
+	printf("Division\nQuotient = %d\n", a/b);
+	break;
+      default:
+	printf("Error! Please type the correct operator\n");
+	break;
+    }          
     return 0;
 }
