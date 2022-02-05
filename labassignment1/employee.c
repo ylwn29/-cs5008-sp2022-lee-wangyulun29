b3b4fb2fedeb2a469e88632322436b62ca99ec16
wangyulun29 <wang.yulun@northeastern.edu>
2022-02-05 00:25:47 -0800

    upload labassignment1-2

--- labassignment1/employee.c
+++ labassignment1/employee.c
@@ -13,20 +13,36 @@ typedef struct employee
 /* Read the employee details using scanf*/
 void readData(int n, employee_t* s)
 {
-    
+    for (int i=0;i<n;i++) {
+        printf("\n");
+        printf("Enter the employeeID, Name, Designation and Department of Employee %d:\n", i+1);  
+        scanf("%d\n%s\n%s\n%s",&(s+i)->empId,(s+i)->Name,(s+i)->Designation,(s+i)->Dept);
+    }
 }
 /* Function to print the employee details*/
 void display(int n, employee_t* s)
 {
-    
-    
+    for (int i=0;i<n;i++) {
+    	printf("\n");
+	printf("The details of Employee %d:\n", i+1);
+    	printf("empId: %d\n", (s+i)->empId);
+	printf("Name: %s\n", (s+i)->Name);
+	printf("Designation: %s\n", (s+i)->Designation);
+	printf("Dept: %s\n", (s+i)->Dept);
+    }
 }
 
 /*----------Main Program--------*/
 int main()
 
 {
-    
+    int n;
+    printf("Enter the number of employees: ");
+    scanf("%d", &n);
+    employee_t *s=(employee_t*)malloc(n*sizeof(employee_t));
+    readData(n,s);
+    display(n,s);
+    free(s);   
     return 0;
 }
 
