881839c14bdae36b3d1754862c7e7de8f8c0bada
pkasaragod <83485423+pkasaragod@users.noreply.github.com>
2022-02-15 14:44:17 -0800

    Add files via upload

---
+++ labassignment3/lab3.c
@@ -0,0 +1,15 @@
+//enter your name here
+//enter your email here
+// C program to check if the integer array contains duplicate elements.
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+
+  int arr[] = {20,7,3,2,12,10,15, 2,2,2,2,4};
+  int size = sizeof(arr) / sizeof(arr[0]);
+
+  //insert your code here
+
+return 0;
+}
