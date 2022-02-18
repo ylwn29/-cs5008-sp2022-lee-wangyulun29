69d9cb1b2154a247405269134a0fd04d4b6d6f08
wangyulun29 <wang.yulun@northeastern.edu>
2022-02-17 17:55:34 -0800

    commit changes

--- labassignment3/lab3.c
+++ labassignment3/lab3.c
@@ -1,8 +1,22 @@
-//enter your name here
-//enter your email here
+//Yulun Wang
+//wang.yulun@northeastern.edu
 // C program to check if the integer array contains duplicate elements.
 #include<stdio.h>
 #include<stdbool.h>
+
+void sort(int* arr, int size) {
+    int temp;
+    for (int i=0; i<size; i++) {
+	for (int j=0; j<size-1; j++) {
+		if (arr[i] < arr[j]) {
+		    temp = arr[i];
+	    	    arr[i] = arr[j];
+		    arr[j] = temp;	    
+		}
+	}
+    }
+    
+}
 
 int main(){
 
@@ -10,6 +24,8 @@ int main(){
   int size = sizeof(arr) / sizeof(arr[0]);
 
   //insert your code here
+  sort(arr,12);
+  printf("%d",arr[11]);
 
 return 0;
 }
