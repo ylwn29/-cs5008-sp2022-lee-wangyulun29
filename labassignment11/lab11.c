ac01ac3141726d14430147f6d03797f0f8b9fa35
wangyulun29 <wang.yulun@northeastern.edu>
2022-04-19 23:51:56 -0700

    Hand in lab11 about dining philosophers problem using threads

--- labassignment11/lab11.c
+++ labassignment11/lab11.c
@@ -1,5 +1,5 @@
-//Enter your name here
-//Enter your email here
+//Enter your name here: yulun Wang
+//Enter your email here: wang.yulun@northeastern.edu
 //gcc -pthread lab11.c -o lab11
 #include <stdio.h>
 #include <unistd.h>
@@ -14,27 +14,43 @@ void *philosopher(void *x)
     int n=*a;//n--philosopher number and chopsticks
     
         /*-----Insert your code here----*/
-    
- 
-    
+    // result may come any order, but make sure all philosophers can eat
+    printf("Philosopher %d is thinking.\n", n);
+    pthread_mutex_lock(&chopstick[n]);
+    pthread_mutex_lock(&chopstick[(n+1)%5]);
+    printf("Philosopher %d is eating using chopstick[%d] and chopstick[%d].\n", n, n, (n+1)%5);
+    sleep(1);
+    printf("Philosopher %d finished eating.\n", n);
+    pthread_mutex_unlock(&chopstick[n]);
+    pthread_mutex_unlock(&chopstick[(n+1)%5]);
+    return 0;    
 }
 
 
 /*------------ Main Program---------*/
 int main()
 {
-    
-    
+    pthread_t threads[5];
+    int i, val[5];
 //initializing the mutex
-    
+    for (i=0;i<5;i++){
+    	pthread_mutex_init(&chopstick[i], NULL);
+    }
     
 //create and run the thread
-    
+    for (i=0;i<5;i++){
+	val[i] = i;
+    	pthread_create(&threads[i], NULL, (void *)philosopher, &val[i]);
+    }
 //wait until all the threads are done
-    
+    for (i=0;i<5;i++){
+    	pthread_join(threads[i], NULL);
+    }
     
 // Destroying the mutex
-    
+    for (i=0;i<5;i++){
+    	pthread_mutex_destroy(&chopstick[i]);
+    }
     
     return 0;
 }
