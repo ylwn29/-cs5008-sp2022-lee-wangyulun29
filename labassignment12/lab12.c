e8d0c8408c924bba1326aec0e48624210553c912
pkasaragod <83485423+pkasaragod@users.noreply.github.com>
2022-04-26 13:58:28 -0700

    Add files via upload

---
+++ labassignment12/lab12.c
@@ -0,0 +1,23 @@
+//enter your email here
+//enter your name here
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+void longestCommonPrefix(char arr[][20], char res[20]){
+    
+    
+    //insert your code here
+    
+    
+    
+    
+}
+
+int main(){
+    
+    
+    
+    
+    return 0;
+}
+
