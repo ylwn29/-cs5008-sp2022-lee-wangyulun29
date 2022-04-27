2ef41166e8524b2b7a638e8aea7fa9ab2b98c025
wangyulun29 <wang.yulun@northeastern.edu>
2022-04-26 23:15:55 -0700

    Hand in lab12 about LCP

--- labassignment12/lab12.c
+++ labassignment12/lab12.c
@@ -3,7 +3,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdbool.h>
-void longestCommonPrefix(char arr[][20], char res[20]){
+void longestCommonPrefix(char arr[][20],int m, char res[20]){
     
     
     //insert your code here
@@ -13,7 +13,7 @@ void longestCommonPrefix(char arr[][20], char res[20]){
     for (i=0;i<n;i++){
         char c=arr[0][i];
 	bool flag=true;
-	for (j=1;j<n;j++){
+	for (j=1;j<m;j++){
 	    if (!(c==arr[j][i])){
 	        flag=false;
 		break;
@@ -33,7 +33,9 @@ int main(){
     int i;
     char arr[5][20]={"aaabc","aaanv","aaamk","aaavf","aaacf"};//input
     char res[20];//output
-    longestCommonPrefix(arr,res);
+    int m=sizeof(arr)/sizeof(arr[0]);//number of rows
+
+    longestCommonPrefix(arr,m,res);
     printf("The LCP is: ");
     for (i=0;i<strlen(res);i++){
         printf("%c",res[i]);
