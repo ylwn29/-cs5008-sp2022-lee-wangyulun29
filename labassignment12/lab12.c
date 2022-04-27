b6fcf730aea3d98a73757aa163a3cc23c9bd698e
wangyulun29 <wang.yulun@northeastern.edu>
2022-04-26 19:00:15 -0700

    Hand in lab12 about finding the longest common prefix

--- labassignment12/lab12.c
+++ labassignment12/lab12.c
@@ -1,5 +1,5 @@
-//enter your email here
-//enter your name here
+//enter your email here: wang.yulun@northeastern.edu
+//enter your name here: Yulun Wang
 #include<stdio.h>
 #include<string.h>
 #include<stdbool.h>
@@ -7,16 +7,38 @@ void longestCommonPrefix(char arr[][20], char res[20]){
     
     
     //insert your code here
-    
-    
-    
-    
+    int index=0;
+    int n=strlen(arr[0]);
+    int i,j;
+    for (i=0;i<n;i++){
+        char c=arr[0][i];
+	bool flag=true;
+	for (j=1;j<n;j++){
+	    if (!(c==arr[j][i])){
+	        flag=false;
+		break;
+	    }
+	}
+	if(flag==true){
+	    res[i]=c;
+	}else{
+	    break;
+	}	
+    }
+    res[i]='\0';
+        
 }
 
 int main(){
-    
-    
-    
+    int i;
+    char arr[5][20]={"aaabc","aaanv","aaamk","aaavf","aaacf"};//input
+    char res[20];//output
+    longestCommonPrefix(arr,res);
+    printf("The LCP is: ");
+    for (i=0;i<strlen(res);i++){
+        printf("%c",res[i]);
+    }
+    printf("\n");
     
     return 0;
 }
